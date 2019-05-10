d76f4c330a8993e635d10741a1c0494550c4b973
jakemiller16 <50153960+jakemiller16@users.noreply.github.com>
2019-05-10 09:50:17 -0700

    initial commit

---
+++ _js/test.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+var x = 42; 
+var y = 54; 
+var z = x+y
+console.log(z); 
+
+// alert(z); 
+console.log(Math.floor(Math.random()*20));
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = (Math.floor(Math.random()*20));
+const sqaure=function(x) {
+    return x * x;
+}; 
+
+console.log(sqaure(12));
